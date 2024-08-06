e4b297defae05490c0489576c5c6072ccbc5b781
MaucalderondelaB <maucalderonrg@gmail.com>
2024-08-06 22:58:07 +0200

    Final version assistant for the simples RAG version

--- simple_rag/helper_function.py
+++ simple_rag/helper_function.py
@@ -1,6 +1,5 @@
 import sys
 import os
-import tqdm as notebook_tqdm
 from typing import List, Union
 
 from langchain_mistralai.chat_models import ChatMistralAI
@@ -10,6 +9,8 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_mistralai import MistralAIEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain.prompts import ChatPromptTemplate
+from langchain.schema import AIMessage
+from langchain.schema.retriever import BaseRetriever
 
 
 # Path to the directory containing config.py
@@ -26,33 +27,37 @@ from config import API_KEY
 
     
 class SimpleRAG:
-    def __init__(self, api_key):
-        self.API_KEY = api_key
-        self.prompt_template = ChatPromptTemplate.from_messages([
+    def __init__(self, api_key: str):
+        self.API_KEY: str = api_key
+        self.prompt_template : ChatPromptTemplate= ChatPromptTemplate.from_messages([
             ("system", "You are an rule assistant for the NFL. Answer the questions based on the context provided. If the answer to the question is not in the context, say \"I don't know\"."),
             ("human", "Context: {context}\n\nQuestion: {question}\nAnswer:")
         ])
+        self.folder_path: str = "/home/mauricio/Documents/Projects/RAG-Mastery/docs"
+        self.llm_model : ChatMistralAI = self.get_llm_model()
+        self.retriever : BaseRetriever 
 
-    def get_llm_model(self):
+    def get_llm_model(self)-> ChatMistralAI:
         return ChatMistralAI(
             model_name="open-mixtral-8x22b", 
             mistral_api_key=self.API_KEY
         )
 
 
-    def load_documents(self, folder_path):
+    def load_documents(self, folder_path: str) -> List[Document]:
         documents = []
         for file in os.listdir(folder_path):
             if file.endswith('.docx') or file.endswith('.pdf') or file.endswith('.txt'):
                 loader = UnstructuredLoader(os.path.join(folder_path, file))
                 documents.extend(loader.load())
-            print("Document loaded lenght: ", len(documents))
-        print("Documents loaded successfully ✅")
-        print(documents[0].metadata.get("filename"))
-        return 
+            
+        print("✅ Documents loaded successfully")
+        print("     Document loaded lenght: ", len(documents))
+        print("     File name: ", documents[0].metadata.get("filename"))
+        return documents
 
 
-    def split_documents(self, documents, chunk_size= 1000, chunk_overlap= 200):
+    def split_documents(self, documents: List[Document], chunk_size: int = 1000, chunk_overlap: int = 200) -> List[Document]:
             try:
                 text_splitter: RecursiveCharacterTextSplitter = RecursiveCharacterTextSplitter(
                     chunk_size=chunk_size,
@@ -61,47 +66,42 @@ class SimpleRAG:
                     separators=["\n\n", "\n", " ", ""]
                 )
                 splits: List[Document] = text_splitter.split_documents(documents)
-                print("Split document successfully ✅")
-                print("Documents split: ", len(splits))
+                print("✅ Split document successfully")
+                print("     Documents split: ", len(splits))
                 return splits
             except Exception as e:
                 print(f"Error splitting documents: {e}")
                 raise
             
 
-    def embed_documents(self, splits):
+    def embed_documents(self, splits: List[Document]) -> BaseRetriever:
         try:
-            embeddings = MistralAIEmbeddings(
+            embeddings: MistralAIEmbeddings = MistralAIEmbeddings(
                 model="mistral-embed",
-                mistral_api_key=API_KEY
-                )
-            vectorstore = FAISS.from_documents(documents=splits, embedding=embeddings)
-            retriever = vectorstore.as_retriever(
+                mistral_api_key=self.API_KEY
+            )
+            vectorstore: FAISS = FAISS.from_documents(documents=splits, embedding=embeddings)
+            retriever: BaseRetriever = vectorstore.as_retriever(
                 search_type="mmr",
                 search_kwargs={"k": 6},
             )
 
-            print("Retriever succesfuly created ✅")
+            print("✅ Retriever successfully created")
             return retriever
         except Exception as e:
-            print(f"Error embeding/retrieving documents {e}")
+            print(f"Error embedding/retrieving documents {e}")
             raise
 
-    def rag_chain(self, question):
-
+    def rag_chain(self, question: str) -> AIMessage:
+        context: List[Document] = self.retriever.invoke(question)
+        context_str: str = "\n".join([doc.page_content for doc in context])
         
-        # Format the messages
-        # Retrieve the relevant documents
-        context = self.retriever.get_relevant_documents(question)
-        context_str = "\n".join([doc.page_content for doc in context])
-        
-        # Format the messages
         messages = self.prompt_template.format_messages(
             context=context_str,
             question=question
         )
         
-        # Generate the answer
-        response = self.llm.invoke(messages)
+        response: AIMessage = self.llm.invoke(messages)
         
         return response
+    
--- simple_rag/main.py
+++ simple_rag/main.py
@@ -1,41 +1,57 @@
 import sys
 import os
+from typing import List
+
 
 # Import your SimpleRAG class and API_KEY
 from helper_function import SimpleRAG, API_KEY
+from langchain.schema.document import Document
+from langchain.schema.retriever import BaseRetriever
+from langchain_mistralai.chat_models import ChatMistralAI
 
+import warnings
+# Suppress the specific warning
+warnings.filterwarnings("ignore", message="Could not download mistral tokenizer")
+import logging
+logging.getLogger().setLevel(logging.WARNING)
+# Suppress HTTP request logs
+logging.getLogger("httpx").setLevel(logging.WARNING)
 
+# Suppress FAISS logs
+logging.getLogger("faiss").setLevel(logging.WARNING)
 
-def main():
-    folder_path = "/home/mauricio/Documents/Projects/RAG-Mastery/data"
+# Suppress pikepdf logs
+logging.getLogger("pikepdf").setLevel(logging.WARNING)
+
+def main() -> None:
+    folder_path: str = "/home/mauricio/Documents/Projects/RAG-Mastery/data"
     
     # Initialize the SimpleRAG
-    rag = SimpleRAG(API_KEY)  # Initialize with None for retriever initially
-
+    rag: SimpleRAG = SimpleRAG(API_KEY)  # Initialize with None for retriever initially
     # Use the instance methods
-    documents = rag.load_documents(folder_path)
-    splits = rag.split_documents(documents)
-    retriever = rag.embed_documents(splits)
+    documents: List[Document] = rag.load_documents(folder_path)
+    splits: List[Document] = rag.split_documents(documents)
+    retriever: BaseRetriever = rag.embed_documents(splits)
 
     # Update the retriever in the rag instance
     rag.retriever = retriever
 
     # Initialize the LLM
-    rag.llm = rag.get_llm_model()
+    rag.llm: ChatMistralAI = rag.get_llm_model()
 
-    print("Welcome to the NFL Rule Assistant!")
-    print("Ask questions about NFL rules, or type 'quit' to exit.")
+    print("\nWelcome to the NFL 🏈 Rule Assistant!")
+    print("Ask questions about NFL rulebook of 2024, or type 'quit' to exit.")
 
     while True:
-        user_input = input("\nYour question: ")
+        user_input: str = input("\n 🔍Your question: ")
         
-        if user_input.lower() == 'quit':
-            print("Thank you for using the NFL Rule Assistant. Goodbye!")
+        if user_input.lower() == 'quit' or user_input.lower() == 'exit':
+            print("Thank you for using the NFL 🏈 Rule Assistant 2024. Goodbye!")
             break
 
         try:
             response = rag.rag_chain(user_input)
-            print("\nAssistant:", response.content)
+            print("\n 🤖 Assistant:", response.content)
         except Exception as e:
             print(f"An error occurred: {e}")
 
